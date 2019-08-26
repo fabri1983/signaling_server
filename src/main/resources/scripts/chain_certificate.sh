339a862147a0da161e2dcec879d97de99bdb5182
Pablo Fabricio Lettieri <fabri1983@gmail.com>
2019-08-26 15:09:24 -0300

    chain_certificate.bat: still can't output files in ASCII or UTF-8

--- src/main/resources/scripts/chain_certificate.sh
+++ src/main/resources/scripts/chain_certificate.sh
@@ -4,9 +4,9 @@
 rm -f rootca.jks localca.jks serverca.jks 2> /dev/null
 rm -f rootca.pem localca.pem serverca.pem 2> /dev/null
 
-echo "================================================="
-echo "Creating fake third-party chain rootca -> localca"
-echo "================================================="
+echo "==================================================="
+echo "Creating fake third-party chain rootca with localca"
+echo "==================================================="
 
 # generate private keys for root and local
 keytool -genkeypair -alias rootca -dname "cn=Local Development (Root)" -validity 1095 -keyalg RSA -keysize 2048 -ext bc:c -keystore rootca.jks -keypass rootcapass -storepass rootcapass
@@ -15,7 +15,7 @@ keytool -genkeypair -alias localca -dname "cn=Local Development" -validity 1095 
 # generate root certificate
 keytool -exportcert -rfc -alias rootca -keystore rootca.jks -storepass rootcapass -file rootca.pem
 
-# generate local certificate signed by root (rootca -> localca)
+# generate chain local certificate signed by root (rootca -> localca)
 keytool -keystore localca.jks -storepass localcapass -certreq -alias localca \
 | keytool -keystore rootca.jks -storepass rootcapass -gencert -alias rootca -ext bc=0 -ext san=dns:ca -rfc > localca.pem
 
@@ -30,7 +30,7 @@ echo "======================================================================="
 # generate private keys for server
 keytool -genkeypair -alias serverca -dname cn=Server -validity 1095 -keyalg RSA -keysize 2048 -keystore local-keystore.jks -keypass servercapass -storepass servercapass
 
-# generate a certificate for server signed by local (rootca -> localca -> serverca)
+# generate chain certificate for server signed by local (rootca -> localca -> serverca)
 keytool -keystore local-keystore.jks -storepass servercapass -certreq -alias serverca \
 | keytool -keystore localca.jks -storepass localcapass -gencert -alias localca -ext ku:c=dig,keyEnc \
 -ext "san=dns:localhost,ip:127.0.0.1,ip:192.168.99.100,ip:172.17.0.2,ip:172.17.0.3" -ext eku=sa,ca -rfc > serverca.pem
